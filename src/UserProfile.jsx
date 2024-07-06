cbd8d94deb4e7fbf829c01d0552f0310c26a2c68
NABET Mohammed <nabetmohammedcontact@gmail.com>
2024-07-06 19:58:29 +0200

    Initial commit

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -8,11 +8,25 @@ function UserProfile() {
         <div
           className="w-16 h-16 bg-cover bg-center bg-no-repeat rounded-full"
           style={{
-            backgroundImage:
-              "url('./maslogo.jpg')",
+            backgroundImage: "url('./maslogo.jpg')",
           }}
         ></div>
-        <span className="my-3">@_Club_Mas</span>
+        <span className="my-3">
+          @_Club_Mas
+          <div className="relative">
+            
+            <svg
+              width="10"
+              height="10"
+              fill="currentColor"
+              className="absolute bottom-2 left-28 w-4 h-4 p-1 -mx-1 -my-1 text-white bg-black rounded-full fill-current"
+              viewBox="0 0 1792 1792"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+            </svg>
+          </div>
+        </span>
 
         <div className="flex gap-10 text-sm">
           <div className="flex flex-col items-center">
@@ -33,9 +47,16 @@ function UserProfile() {
           Edit profile
         </button>
 
-        <p className="mb-3">Description about me goes here</p>
+        <p className="mb-3 text-center">
+          Maghreb Association Sportive de Fès ⚫️🟡
+          <br />
+          <p className="text-xs text-center	">
+            مرحبا بكم بالصفحة الرسمية لنادي المغرب الرياضي الفاسي <br />
+            Bienvenue sur La Page Officielle du MAS🐯
+          </p>
+        </p>
       </div>
-      <Posthome/>
+      <Posthome />
     </div>
   );
 }
