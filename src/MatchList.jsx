721a1ff8c151f57300a4b2ba73edd7683bb848d8
NABET Mohammed <nabetmohammedcontact@gmail.com>
2024-07-07 03:00:27 +0200

    Initial commit

--- src/MatchList.jsx
+++ src/MatchList.jsx
@@ -90,14 +90,14 @@ const matchData = [
 function MatchList() {
   return (
     <div className="max-w-2xl mx-auto">
-      <div className="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8  ">
         <div className="flex justify-between items-center mb-4">
-          <h3 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+          <h3 className="text-xl font-bold leading-none text-gray-900 ">
             List Match
           </h3>
           <a
             href="#"
-            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+            className="text-sm font-medium text-blue-600 hover:underline "
           >
             View all
           </a>
@@ -105,7 +105,7 @@ function MatchList() {
         <div className="flow-root">
           <ul
             role="list"
-            className="divide-y divide-gray-200 dark:divide-gray-700"
+            className="divide-y divide-gray-200 "
           >
             {matchData.map((match) => (
               <li key={match.id} className="py-3 sm:py-4">
@@ -118,10 +118,10 @@ function MatchList() {
                     />
                   </div>
                   <div className="flex-1 min-w-0 text-center">
-                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    <p className="text-sm font-medium text-gray-900 truncate ">
                       {match.date}
                     </p>
-                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    <p className="text-sm text-gray-500 truncate ">
                       {match.email}
                     </p>
                   </div>
