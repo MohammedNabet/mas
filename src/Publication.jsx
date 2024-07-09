f57ca94082b542192f0a1165dd83b66d6c73d0c5
NABET Mohammed <nabetmohammedcontact@gmail.com>
2024-07-09 22:01:03 +0200

    Initial commit

--- src/Publication.jsx
+++ src/Publication.jsx
@@ -4,31 +4,51 @@ import React from "react";
 export default function Publication() {
   const [publication, setPublication] = React.useState([
     {
+      id: 3,
+      title: "botola inwi pro",
+      img: "./botola.png",
+      pay1: "./m.jpeg",
+      pay2: "./inwi.jpeg",
+      pay3: "",
+      right: "-right-6",
+    },
+    {
       id: 1,
       title: "Coupe du Monde 2026",
       img: "./coup2026.png",
       pay1: "./canada.jpg",
       pay2: "./amec.jpeg",
       pay3: "./mex.jpeg",
+      right: "-right-24",
     },
     {
       id: 2,
+      title: "CUB africa 2025",
+      img: "./cupaff.png",
+      pay1: "./m.jpeg",
+      pay2: "",
+      pay3: "",
+      right: "-right-24",
+    },
+
+    {
+      id: 4,
       title: "Premier League",
       img: "./an1.png",
       pay1: "./angla.jpeg",
       pay2: "",
       pay3: "",
+      right: "-right-24",
     },
-
     {
-      id: 2,
+      id: 5,
       title: "La Liga",
       img: "./l01.png",
       pay1: "./ispan.jpeg",
       pay2: "",
       pay3: "",
+      right: "-right-24",
     },
-
     // ... other posts
   ]);
 
@@ -41,7 +61,7 @@ export default function Publication() {
               <img
                 alt="moto"
                 src={user.img}
-                className="absolute w-auto h-36 mb-2 -right-24 -bottom-0"
+                className={`absolute w-auto h-36 mb-2 ${user.right}  -bottom-0`}
               />
             )}
             <div className="w-4/6">
@@ -52,11 +72,10 @@ export default function Publication() {
                 Detail is not an obsession, it is the very essence of
                 perfection.
               </p>
-
               <br />
               <div className="flex -space-x-2">
                 {user.pay1 && (
-                  <a href="#" className="">
+                  <a href="#">
                     <img
                       className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
                       src={user.pay1}
@@ -65,7 +84,7 @@ export default function Publication() {
                   </a>
                 )}
                 {user.pay2 && (
-                  <a href="#" className="">
+                  <a href="#">
                     <img
                       className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
                       src={user.pay2}
@@ -74,7 +93,7 @@ export default function Publication() {
                   </a>
                 )}
                 {user.pay3 && (
-                  <a href="#" className="">
+                  <a href="#">
                     <img
                       className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
                       src={user.pay3}
