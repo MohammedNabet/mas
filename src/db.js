f4e7901c78453b4810132c3d0a4a543e2575d550
NABET Mohammed <nabetmohammedcontact@gmail.com>
2024-07-27 00:37:20 +0200

    Initial commit

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: "mas-postgres",
+  database: "",
   password: process.env.POSTGRES_PASSWORD,
   port: 5432, // default PostgreSQL port
   ssl: {
