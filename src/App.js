cf68e876890f4e659a03b48dbdc73f1d56c77504
NABET Mohammed <nabetmohammedcontact@gmail.com>
2024-07-28 00:51:47 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-const express = require("express");
-const app = express();
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import InsertUser from "./InsertUser";
 
-require("dotenv").config();
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/insert-user" element={<InsertUser />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
+  );
+}
 
-app.use(express.json());
-
-const bookRouter = require("./routes/book.router");
-
-app.use("/api/v1/books", bookRouter);
-
-app.listen(process.env.PORT, () =>
-  console.log("Server is running on port 5000")
-);
+export default App;
